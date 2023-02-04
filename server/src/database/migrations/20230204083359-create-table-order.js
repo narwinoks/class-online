a136f9e211c923894706e8d116648e57ad4e7484
narwinoks <narnowin195@gmail.com>
2023-02-04 16:40:14 +0700

    migration model order

---
+++ server/src/database/migrations/20230204083359-create-table-order.js
@@ -0,0 +1,57 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     */
+    await queryInterface.createTable("order", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      status: {
+        type: Sequelize.STRING,
+        defaultValue: "pending",
+        allowNull: true,
+      },
+      course_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      snap_url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      meta_data: {
+        type: Sequelize.JSON,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     */
+    await queryInterface.dropTable("order");
+  },
+};
---
+++ server/src/models/Order.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+  const Order = sequelize.define(
+    "Order",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNUll: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        default: "pending",
+      },
+      course_id: {
+        type: DataTypes.INTEGER,
+        allowNUll: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNUll: false,
+      },
+      snap_url: {
+        type: DataTypes.STRING,
+        allowNUll: false,
+      },
+      meta_data: {
+        type: DataTypes.STRING,
+        allowNUll: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "created_at",
+        allowNUll: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "updated_at",
+        allowNUll: false,
+      },
+    },
+    {
+      tableName: "order",
+      timestamps: true,
+    }
+  );
+  return Order;
+};

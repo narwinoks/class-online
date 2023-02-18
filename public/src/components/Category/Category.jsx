ef3e8714a60959881a512ad978c166bf5eb6d159
narwinoks <narnowin195@gmail.com>
2023-02-18 22:19:06 +0700

    updated

--- public/src/components/Category/Category.jsx
+++ public/src/components/Category/Category.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import IconsTreasure from "../../assets/icons8-mongodb-48.png";
+import "./Category.css";
+import { Link } from "react-router-dom";
 
 const Category = () => {
   return (
-    <section className="categories">
+    <section className="categories bg-light">
       <div className="container">
-        <h1 className="mt-5 mb-5">Eksplorasi Materi Codepolitan</h1>
-        <div className="row">
+        <div className="row mt-5 mb-5">
+          <div className="col-md-6">
+            <h1 className="h2">Eksplorasi Materi Codepolitan</h1>
+          </div>
+          <div className="col-md-6 text-end text-success">
+            <Link to="/home">Lihat Semua</Link>
+          </div>
           <div className="col-md-3">
             <div className="px-2 my-3">
-              <a className="link" href="/library/?type=mongodb">
+              <a className="link" href="">
                 <div
                   className="card border-0 shadow mb-4"
                   style={{ borderRadius: "12px" }}
@@ -20,7 +27,10 @@ const Category = () => {
                         <img src={IconsTreasure} alt="1" />
                       </div>
                       <div className="col-8 my-auto">
-                        <h5 className="title-category" style={{ fontSize: "medium", marginBottom: "0px" }}>
+                        <h5
+                          className="title-category"
+                          style={{ fontSize: "medium", marginBottom: "0px" }}
+                        >
                           MongoDB
                         </h5>
                         <span style={{ fontSize: "small" }}>2 Kelas</span>
--- public/src/page/Home/Home.jsx
+++ public/src/page/Home/Home.jsx
@@ -3,15 +3,21 @@ import Navbar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import RoadMap from "../../components/RoadMap/RoadMap";
 import Category from "../../components/Category/Category";
+import Popular from "../../components/Popular/Popular";
+import Footer from "../../components/Footer/Footer";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Banner></Banner>
-      <RoadMap></RoadMap>
-      <Category></Category>
+      <div className="bg-light">
+        <Banner></Banner>
+        <RoadMap></RoadMap>
+        <Category></Category>
+        <Popular></Popular>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
